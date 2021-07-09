992e080bd63d51e004c928634177a8cab2a778aa
Elizabeth Mangano <79860046+emangano2816@users.noreply.github.com>
2021-07-09 09:57:57 -0400

    Modified code: routes/api/notes.js: cleaned up code; wrote helper function for writing JSON files; modified DELETE route to read and write JSON files db/db.json: cleared out existing notes.

--- routes/api/notes.js
+++ routes/api/notes.js
@@ -1,19 +1,30 @@
 const fs = require('fs');
 const express = require('express');
 const router = express.Router();
-// const notes = require('../../db/db.json');
 const path = require('path');
 const uuid = require('uuid');
 
+//helper function to write files
+function jsonWriter(jsobject) {
+    fs.writeFile(path.join(__dirname, '../../db/db.json'), JSON.stringify(jsobject), err => {
+        if (err) {
+            console.log("Error writing file:", err);
+        } else {
+            console.log("Successfully wrote file");
+        }
+    });
+}
+
 //Route for GET /api/notes - returns all notes
 router.get('/', (req, res) => {
-
+    //read JSON file
     fs.readFile(path.join(__dirname, '../../db/db.json'), 'utf8', (err, jsonstring) =>{
         if (err) {
             console.log("Error reading file:", err);
             return
         } 
         try {
+            //parse JSON string into a JS object
             const notes = JSON.parse(jsonstring);
             console.log("Existing notes include:", notes);
             res.json(notes);
@@ -24,41 +35,11 @@ router.get('/', (req, res) => {
     })
 });
 
-// Route for GET /api/notes/:id - returns single note by ID
-router.get('/:id', (req,res) => {
-    //retrieve and store requested id
-    const chosenNote = req.params.id;
-    console.log(chosenNote);
-    
-    //read JSON file
-    fs.readFile(path.join(__dirname, '../../db/db.json'), 'utf8', (err, jsonstring) => {
-        if (err) {
-            console.log("Error reading File:", err);
-            return;
-        }
-        try {
-            const notes = JSON.parse(jsonstring);
-            //search notes and find requested note by id - if id exists return it
-            for (let i = 0; i < notes.length; i++) {
-                if (chosenNote === notes[i].id) {
-                return res.json(notes[i]);
-                } else {
-                    return res.json(false);
-                }
-            } 
-        }
-        catch (err) {
-            console.log("Error parsing JSON string:", err)
-        }
-    })
-})
-
 //Route for POST /api/notes - takes user input and adds note to notes
 router.post('/', (req, res) => {
     //store information to post, set the ID property
     const newnote = req.body;
     newnote.id = uuid.v4();
-    console.log(newnote);
 
     //read JSON file
     fs.readFile(path.join(__dirname, '../../db/db.json'), 'utf8', (err, jsonstring) => {
@@ -67,47 +48,56 @@ router.post('/', (req, res) => {
             return;
         }
         try {
-            //return db.JSON contents as JS object
+            //parse JSON string into a JS object
             const notes = JSON.parse(jsonstring);
             
             //add the object
             notes.push(newnote);
             console.log("Saved notes:", notes);
-            //write results to file
-            fs.writeFile(path.join(__dirname, '../../db/db.json'), JSON.stringify(notes), err => {
-                if (err) {
-                    console.log("Error writing file:", err);
-                } else {
-                    console.log("Successfully wrote file")
-                }
-            });
 
-            //return the added note
+            //write results to file
+            jsonWriter(notes);
+
+            //return notes
             res.json(notes);
         }
         catch (err) {
-            console.log("Error parsing JSON string:", err)
+            console.log("Error parsing JSON string:", err);
         }
     })
-
-
 })
 
 
 //Route for DELETE - removes note from notes by ID
 router.delete('/:id', (req, res) => {
-    //look up note to delete; if doesn't exist return 404
-    const deletenote = notes.find(note => note.id === req.params.id);
+    //read JSON file
+    fs.readFile(path.join(__dirname, '../../db/db.json'), 'utf8', (err, jsonstring) => {
+        if (err) {
+            console.log("Error reading File:", err);
+            return;
+        }
+        try {
+            //parse JSON string into a JS object
+            const notes = JSON.parse(jsonstring);
 
-    console.log(req.params.id);
-    if (!deletenote) return res.status(404).send('There was an issue deleting the requested note');
+            //look up note to delete; if doesn't exist return 404
+            const deletenote = notes.find(note => note.id === req.params.id);
+            if (!deletenote) return res.status(404).send('There was an issue deleting the requested note');
 
-    //delete the note
-    const index = notes.indexOf(deletenote);
-    notes.splice(index,1);
+            //delete the note
+            const index = notes.indexOf(deletenote);
+            notes.splice(index,1);
 
-    //return the deleted note
-    res.send(deletenote);
-})
+            //write results to file
+            jsonWriter(notes);
+
+            //return the deleted note
+            res.json(deletenote);
+        }
+        catch (err) {
+            console.log("Error parsing JSON string:", err);
+        }
+    })
+});
 
 module.exports = router;
